46dfb176f3aac1ef13546df31c079598b7edcba1
Tingan Ho <tingan87@gmail.com>
2013-02-02 19:47:37 +0800

    Added basic test script

---
+++ test/test.js
@@ -0,0 +1,14 @@
+var chai = require( 'chai' ),
+    expect = chai.expect,
+    assert = chai.assert;
+
+describe('Authentication', function() {
+
+
+    describe('GET /login', function() {
+        it('Test is ok', function() {
+            assert.ok(true, 'Test the test');
+        });
+
+    });
+});

4c00d5401638ed1c3c51e60cbef6370c72969479
Tingan Ho <tingan87@gmail.com>
2013-02-03 17:07:09 +0800

    Added jshint

---
+++ test/test_modrewrite.js
@@ -0,0 +1,45 @@
+var chai = require( 'chai' ),
+    expect = chai.expect,
+    assert = chai.assert,
+    http   = require('http'),
+    grunt = require('grunt');
+
+describe('Authentication', function() {
+
+  describe('GET /friends', function() {
+
+    it('should response to one level path', function(done) {
+      http.get('http://localhost:9001/test', function(res){
+        res.setEncoding('utf8');
+        res.on('data', function(chunk){
+          expect(chunk).to.be.a('string');
+          done();
+        });
+      });
+    });
+
+    it('should be able normalize relative paths', function(done) {
+      var options = {
+        hostname: 'localhost',
+        port: 9001,
+        path: '/test/style.css',
+        method: 'GET',
+        headers: {
+          'Referer': 'http://localhost:9001/test/2'
+        }
+      };
+
+      var req = http.request(options, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+          expect(/#relative-paths-get-normalized/.test(chunk)).is.true;
+          done();
+        });
+      });
+      req.end();
+
+    });
+
+
+  });
+});

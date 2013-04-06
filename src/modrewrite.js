9c21c45bc2cf6774f6ef286a8078ec072ed07126
Tingan Ho <tingan87@gmail.com>
2013-04-06 22:11:53 +0800

    Added so that connect-modrewrite rewrites in abscent rules

--- src/modrewrite.js
+++ src/modrewrite.js
@@ -23,12 +23,31 @@ module.exports = function(rules) {
 
   return function(req, res, next) {
 
+    // Some request are not assets request, which means they don't
+    // have an HTTP referer. We only normalize path which are assets
+    if(typeof req.headers.referer !== 'undefined') {
+      // Split URLs for later normalization
+      var referersSplits = req.headers.referer.split('/'),
+          urlSplits = req.url.substr(1).split('/'); // substr(1) is there because the string begins with /
+      // Remove hostname
+      referersSplits.splice(0, 3);
+      // Remove the last part of the referer since it is not
+      // supposed to be used in the normalization process
+      referersSplits.pop();
+      // Normalization process
+      urlSplits.forEach(function(value, index) {
+        if(value === referersSplits[index]) {
+          urlSplits.splice(index, 1);
+        } else {
+          return false;
+        }
+      });
+      // Join back all splits
+      req.url = '/' + urlSplits.join('/');
+    }
+
     rules.some(function(rewrite) {
 
-      // Some request are not assets request, which means they don't
-      // have an HTTP referer. We only normalize path which are assets
-      if(typeof req.headers.referer === 'undefined') {
-
         // Rewrite Url
         if(rewrite.regex.test(req.url) && rewrite.inverted) {
           return rewrite.last;
@@ -41,43 +60,6 @@ module.exports = function(rules) {
           req.url = rewrite.replace;
           return rewrite.last;
         }
-
-      // Else normalize path
-      } else {
-
-        // Split URLs for later normalization
-        var referersSplits = req.headers.referer.split('/'),
-            urlSplits = req.url.substr(1).split('/'); // substr(1) is there because the string begins with /
-        // Remove hostname
-        referersSplits.splice(0, 3);
-        // Remove the last part of the referer since it is not
-        // supposed to be used in the normalization process
-        referersSplits.pop();
-        // Normalization process
-        urlSplits.forEach(function(value, index) {
-          if(value === referersSplits[index]) {
-            urlSplits.splice(index, 1);
-          } else {
-            return false;
-          }
-        });
-        // Join back all splits
-        req.url = '/' + urlSplits.join('/');
-
-        // Rewrite Url
-        if(rewrite.regex.test(req.url) && rewrite.inverted) {
-          return rewrite.last;
-        } else if(!rewrite.regex.test(req.url) && rewrite.inverted) {
-          res.setHeader('Location', 'http://' + req.headers.host + rewrite.replace);
-          req.url = rewrite.replace;
-          return rewrite.last;
-        } else if(rewrite.regex.test(req.url)) {
-          res.setHeader('Location', 'http://' + req.headers.host + rewrite.replace);
-          req.url = rewrite.replace;
-          return rewrite.last;
-        }
-
-      } // End of normalization
 
     });
 

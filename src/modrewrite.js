cfba3156f837835031f0fd2d4f4c0bbf769a7f18
Tingan Ho <tingan87@gmail.com>
2013-04-10 12:40:41 +0800

    Fixed use strict mode

--- src/modrewrite.js
+++ src/modrewrite.js
@@ -1,5 +1,7 @@
 module.exports = function(rules, normalize) {
 
+  'use strict';
+
   rules = (rules || []).map(function(rule) {
 
     var parts = rule.replace(/\s+|\t+/g, ' ').split(' ');
@@ -32,35 +34,36 @@ module.exports = function(rules, normalize) {
     return normalizable;
   }
 
+  function normalizeUrl(req) {
+    // Split URLs for later normalization
+    var referersSplits = req.headers.referer.split('/'),
+        urlSplits = req.url.substr(1).split('/'); // substr(1) is there because the string begins with /
+    // Remove hostname
+    referersSplits.splice(0, 3);
+    // Remove the last part of the referer since it is not
+    // supposed to be used in the normalization process
+    referersSplits.pop();
+    // Normalization process
+    urlSplits.forEach(function(value, index) {
+      if(value === referersSplits[index]) {
+        urlSplits.splice(index, 1);
+      } else {
+        return false;
+      }
+    });
+    // Join back all splits
+    req.url = '/' + urlSplits.join('/');
+  }
+
   return function(req, res, next) {
 
     // Some request are not assets request, which means they don't
     // have an HTTP referer. We only normalize path which are assets
     if(typeof req.headers.referer !== 'undefined') {
 
-      function normalizeUrl() {
-        // Split URLs for later normalization
-        var referersSplits = req.headers.referer.split('/'),
-            urlSplits = req.url.substr(1).split('/'); // substr(1) is there because the string begins with /
-        // Remove hostname
-        referersSplits.splice(0, 3);
-        // Remove the last part of the referer since it is not
-        // supposed to be used in the normalization process
-        referersSplits.pop();
-        // Normalization process
-        urlSplits.forEach(function(value, index) {
-          if(value === referersSplits[index]) {
-            urlSplits.splice(index, 1);
-          } else {
-            return false;
-          }
-        });
-        // Join back all splits
-        req.url = '/' + urlSplits.join('/');
-      }
       if(normalize) {
         if(isNormalizable(req.url)) {
-          normalizeUrl();
+          normalizeUrl(req);
         }
       }
 

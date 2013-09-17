01c2881787cc141028ea61277f0facf45f7e7311
Marco Lehmann <m99@gmx.li>
2013-09-17 17:34:52 +0200

    added support for query strings in proxy mode

--- src/modrewrite.js
+++ src/modrewrite.js
@@ -57,8 +57,8 @@ module.exports = function(rules) {
         _next = false;
         return true;
       } else if(rewrite.regex.test(req.url) && rewrite.proxy) {
-        var opts     = url.parse(req.url.replace(rewrite.regex, rewrite.replace));
-        opts.path    = opts.pathname + '/';
+        var opts     = url.parse(req.url.replace(rewrite.regex, rewrite.replace));        
+        opts.path    = opts.pathname + '/' + opts.search;
         opts.method  = req.method;
         opts.headers = req.headers;
         var via = '1.1 ' + require('os').hostname();

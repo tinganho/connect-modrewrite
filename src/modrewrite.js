37eecda26d9f83adea0c3d01ed47d77f706ebafb
Tingan Ho <tingan87@gmail.com>
2013-11-02 13:09:04 +0800

    Fixes #24

--- src/modrewrite.js
+++ src/modrewrite.js
@@ -59,7 +59,12 @@ module.exports = function(rules) {
       } else if(rewrite.regex.test(req.url) && rewrite.proxy) {
         var opts     = url.parse(req.url.replace(rewrite.regex, rewrite.replace));
         var query    = (opts.search != null) ? opts.search : '';
-        opts.path    = opts.pathname + '/' + query;
+        if(query === '') {
+          opts.path = opts.pathname;
+        }
+        else {
+          opts.path = opts.pathname + '/' + query;
+        }
         opts.method  = req.method;
         opts.headers = req.headers;
         var via = '1.1 ' + require('os').hostname();

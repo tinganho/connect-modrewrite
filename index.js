5be24de3c9436f3115cbe787aa9671adbc44d713
Tingan Ho <tingan87@gmail.com>
2014-02-24 18:16:46 +0800

    Broke out request object parser

--- index.js
+++ index.js
@@ -4,7 +4,95 @@
  */
 
 var url = require('url')
-  , querystring = require('querystring');
+  , querystring = require('querystring')
+  , httReq = require('http').request
+  , httpsReq = require('https').request
+  , via = '1.1 ' + require('os').hostname();
+
+/**
+ * Export `API`
+ */
+
+module.exports = function(rules) {
+  // Parse the rules to get flags, replace and match pattern
+  rules = _parse(rules);
+
+  return function(req, res, next) {
+    var protocol = req.connection.encrypted || req.header('x-forwarded-proto') == 'https' ? 'https' : 'http'
+      , callNext = true;
+
+    rules.some(function(rule) {
+      var location = protocol + '://' + req.headers.host + req.url.replace(rule.regex, rule.replace);
+        , match = rule.regex.test(req.url);
+
+      // If not match
+      if(!match) {
+        // Inverted rewrite
+        if(rule.inverted) {
+          req.url = rule.replace;
+          return rule.last;
+        }
+
+        return false;
+      }
+
+      // Type
+      if(rule.type) {
+        res.setHeader('Content-Type', rule.type);
+      }
+
+      // Gone
+      if(rule.gone) {
+        res.writeHead(410);
+        res.end();
+        callNext = false;
+        return true;
+      }
+
+      // Forbidden
+      if(rule.forbidden) {
+        res.writeHead(403);
+        res.end();
+        callNext = false;
+        return true;
+      }
+
+      // Proxy
+      if(rule.proxy) {
+        _proxy();
+        callNext = false;
+        return true;
+      }
+
+      // Redirect
+      if(rule.redirect) {
+        res.writeHead(rule.redirect, {
+          Location : location
+        });
+        res.end();
+        callNext = false;
+        return true;
+      }
+
+      // Rewrite
+      if(!rule.inverted) {
+        req.url = req.url.replace(rule.regex, rule.replace);
+        return rule.last;
+      }
+    });
+
+    // Add to query object
+    if(/\?.*/.test(req.url)) {
+      req.params = req.query = querystring.parse(/\?(.*)/.exec(req.url)[1]);
+    }
+
+    if(callNext) {
+      next();
+    }
+
+  };
+};
+
 
 /**
  * Get flags from rule rules
@@ -15,7 +103,7 @@ var url = require('url')
  */
 
 function _parse(rules) {
-  rules = (rules || []).map(function(rule) {
+  return (rules || []).map(function(rule) {
     var parts = rule.replace(/\s+|\t+/g, ' ').split(' ');
     var flags = '', flagRegex = /\[(.*)\]$/;
     if(flagRegex.test(rule)) {
@@ -23,7 +111,7 @@ function _parse(rules) {
     }
 
     // Check inverted urls
-    var inverted = parts[0].substr(0,1) === '!';
+    var inverted = parts[0].substr(0, 1) === '!';
     if(inverted) {
       parts[0] = parts[0].substr(1);
     }
@@ -42,100 +130,55 @@ function _parse(rules) {
   });
 };
 
-module.exports = function(rules) {
-  rules = _parse(rules);
 
-  return function(req, res, next) {
-    var protocol = req.connection.encrypted || req.header('x-forwarded-proto') == 'https' ? 'https' : 'http'
-      , request = require(protocol).request
-      , _next = true;
+function _proxy(rule, req, res, protocol) {
+  var opts = _getRequestOpts(req, rule);
+    , request = protocol === 'http' : httpReq : httpsReq;
 
-    rules.some(function(rule) {
-      var location = protocol + '://' + req.headers.host + req.url.replace(rule.regex, rule.replace);
-        , match = rule.regex.test(req.url);
-
-
-      if(!match) {
-        if(rule.inverted) {
-          req.url = rule.replace;
-          return rule.last;
-        }
-        return false;
-      }
-
-      // Rewrite Url
-      if(rule.type) {
-        res.setHeader('Content-Type', rule.type);
-      }
-      if(rule.gone) {
-        res.writeHead(410);
-        res.end();
-        _next = false;
-        return true;
-      }
-      if(rule.forbidden) {
-        res.writeHead(403);
-        res.end();
-        _next = false;
-        return true;
-      }
-      if(match && rule.proxy) {
-        var opts     = url.parse(req.url.replace(rule.regex, rule.replace));
-        var query    = (opts.search != null) ? opts.search : '';
-        if(query) {
-          opts.path = opts.pathname + query;
-        }
-        opts.method  = req.method;
-        opts.headers = req.headers;
-        var via = '1.1 ' + require('os').hostname();
-        if(req.headers.via) {
-           via = req.headers.via + ', ' + via;
-        }
-        opts.headers.via = via;
-
-        delete opts.headers['host'];
-
-        var _req = request(opts, function (_res) {
-          _res.headers.via = via;
-          res.writeHead(_res.statusCode, _res.headers);
-          _res.on('error', function (err) {
-            next(err);
-          });
-          _res.pipe(res);
-        });
-        _req.on('error', function (err) {
-          next(err);
-        });
-        if(!req.readable) {
-          _req.end();
-        } else {
-          req.pipe(_req);
-        }
-        _next = false;
-        return true;
-      }
-      if(rule.redirect) {
-        res.writeHead(rule.redirect, {
-          Location : location
-        });
-        res.end();
-        _next = false;
-        return true;
-      }
-      if(!rule.inverted) {
-        req.url = req.url.replace(rule.regex, rule.replace);
-        return rule.last;
-      }
+  var pipe = request(opts, function (_res) {
+    pipe.headers.via = via;
+    res.writeHead(_res.statusCode, _res.headers);
+    pipe.on('error', function (err) {
+      next(err);
     });
+    pipe.pipe(res);
+  });
+  pipe.on('error', function (err) {
+    next(err);
+  });
+  if(!req.readable) {
+    pipe.end();
+  }
+  else {
+    req.pipe(pipe);
+  }
+};
 
-    // Add to query object
-    if(/\?.*/.test(req.url)) {
-      req.params = req.query = querystring.parse(/\?(.*)/.exec(req.url)[1]);
-    }
+/**
+ * Get request options
+ *
+ * @param {HTTPRequest} req
+ * @param {Object} rule
+ * @return {Object}
+ * @api private
+ */
 
-    if(_next) {
-      next();
-    }
+function _getRequestOpts(req, rule) {
+  var opts = url.parse(req.url.replace(rule.regex, rule.replace))
+    , query = (opts.search != null) ? opts.search : '';
 
-  };
-}
+  if(query) {
+    opts.path = opts.pathname + query;
+  }
+  opts.method  = req.method;
+  opts.headers = req.headers;
+  var via = '1.1 ' + hostname;
+  if(req.headers.via) {
+     via = req.headers.via + ', ' + via;
+  }
+  opts.headers.via = via;
+
+  delete opts.headers['host'];
+
+  return opts;
+};

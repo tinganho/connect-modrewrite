4dcb4d6cf2d0b944bab6f63487ff0b6a8d4d7cce
Tingan Ho <tingan87@gmail.com>
2014-02-24 19:26:11 +0800

    Added test for regular rewrites

--- test/test.js
+++ test/test.js
@@ -1,17 +1,41 @@
 
-var modRewrite = require('../');
+/**
+ * Module dependencies.
+ */
+var modRewrite = require('../')
+  , sinon = require('sinon')
+  , chai = require('chai')
+  , expect = require('chai').expect;
 
+/**
+ * Plugins.
+ */
 
+var sinonChai = require('sinon-chai');
+chai.should();
+chai.use(sinonChai);
+
+/**
+ * Specs.
+ */
 
 describe('Connect-modrewrite', function() {
   describe('last flag', function() {
     it('should not go to next rewrite rule', function() {
       var middleware = modRewrite(['/a /b [L]', '/a /c']);
       var req = {
-        connection { encrypted : false },
-        header : function() {}
+        connection : { encrypted : false },
+        header : function() {},
+        headers : { host : 'test.com' },
+        url : '/a'
       };
-      middleware();
+      var res = {
+        writeHead : sinon.spy(),
+        end : sinon.spy()
+      };
+      var next = sinon.spy();
+      middleware(req, res, next);
+      expect(req.url).to.equal('/b');
     });
   });
 });
